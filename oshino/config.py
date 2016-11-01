397c98064833c03aa7c40e93cc61211acc15c76e
Šarūnas Navickas <sarunas.navickas@adform.com>
2016-11-01 16:29:44 +0200

    Config objects

--- oshino/config.py
+++ oshino/config.py
@@ -4,7 +4,35 @@ import yaml
 from jinja2 import Template
 
 
-class Config(object):
+class ConfigBase(object):
+
+    """
+    Base config class
+    """
+
+    def is_valid(self):
+        return True
+
+
+class RiemannConfig(ConfigBase):
+
+    """
+    Riemann Config object
+    """
+
+    def __init__(self, cfg):
+        self._data = cfg
+
+    @property
+    def host(self):
+        return self._data["host"]
+
+    @property
+    def port(self):
+        return int(self._data["port"])
+
+
+class Config(ConfigBase):
 
     """
     Config object
@@ -12,6 +40,15 @@ class Config(object):
 
     def __init__(self, cfg):
         self._data = cfg
+        self.is_valid()
+
+    def is_valid(self):
+        checks = [self.riemann.is_valid]
+        return all([c() for c in checks])
+
+    @property
+    def riemann(self):
+        return RiemannConfig(self._data["riemann"])
 
 
 def load(config_file):
