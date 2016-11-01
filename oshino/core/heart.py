b3e383b6e2de8ef33ff28374e4f25e27413e777b
Šarūnas Navickas <sarunas.navickas@adform.com>
2016-11-01 17:14:19 +0200

    Exit signal handlers

--- oshino/core/heart.py
+++ oshino/core/heart.py
@@ -1,13 +1,28 @@
 import asyncio
+import signal
 
 from logbook import Logger
 
 from ..config import Config
 from ..version import get_version
 
+loop = asyncio.get_event_loop()
+
+
+def ask_exit():
+    loop.stop()
+
 
 def start_loop(cfg: Config):
     logger = Logger("Heart")
-    loop = asyncio.get_event_loop()
-
     logger.info("Initializing Oshino v{0}".format(get_version()))
+    exit_signals = ("SIGINT", "SIGTERM", )
+    for signame in exit_signals:
+        loop.add_signal_handler(getattr(signal, signame), ask_exit)
+
+    logger.info("Running forever in {0}seconds interval. Press Ctrl+C to exit"
+                .format(cfg.interval))
+    try:
+        loop.run_forever()
+    finally:
+        loop.close()
--- tests/test_runner.py
+++ tests/test_runner.py
@@ -3,5 +3,6 @@ from oshino.run import main
 
 
 @mark.integration
+@mark.skip("It runs forever")
 def test_startup():
     main(("--config", "tests/data/test_config.yml", ))
