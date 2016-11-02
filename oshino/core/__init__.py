b838ffa3fdf107a9a91671ee6eab8fff0bf7c198
Šarūnas Navickas <sarunas.navickas@adform.com>
2016-11-02 17:45:13 +0200

    Choose heartbeat ttl by interval

--- oshino/core/__init__.py
+++ oshino/core/__init__.py
@@ -1,5 +1,6 @@
-def send_heartbeat(event_fn, logger):
+def send_heartbeat(event_fn, logger, ttl):
     logger.debug("Sending heartbeat")
     event_fn(metric_f=1.0,
              service="oshino.heartbeat",
-             tags=["oshino", "heartbeat"])
+             tags=["oshino", "heartbeat"],
+             ttl=ttl)
--- oshino/core/heart.py
+++ oshino/core/heart.py
@@ -28,7 +28,7 @@ async def main_loop(cfg: Config, logger: Logger):
     client = QueuedClient(transport)
     agents = list(map(lambda x: (x.instance, x), cfg.agents))
     while True:
-        send_heartbeat(client.event, logger)
+        send_heartbeat(client.event, logger, cfg.interval)
         for agent, agent_cfg in agents:
             tags = [agent_cfg.tag] if agent_cfg.tag else None
             event_fn = partial(client.event,
