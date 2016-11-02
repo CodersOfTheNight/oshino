6a8cead1b73367acb287dffb96fd661b3c31ccfb
Šarūnas Navickas <sarunas.navickas@adform.com>
2016-11-02 07:35:40 +0200

    Host and port for consul agent

--- oshino/agents/consul_agent.py
+++ oshino/agents/consul_agent.py
@@ -2,4 +2,11 @@ from . import Agent
 
 
 class ConsulAgent(Agent):
-    pass
+
+    @property
+    def host(self):
+        return self._data.get("host", "localhost")
+
+    @property
+    def port(self):
+        return int(self._data.get("port", 8500))
