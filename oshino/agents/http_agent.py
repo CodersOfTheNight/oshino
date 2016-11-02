11ce6af47b7c810fdd03560df2dfa1f3967ea05a
Šarūnas Navickas <sarunas.navickas@adform.com>
2016-11-02 06:44:22 +0200

    Http agent

---
+++ oshino/agents/http_agent.py
@@ -0,0 +1,63 @@
+import aiohttp
+
+from time import time
+from . import Agent
+
+
+class Success(object):
+
+    def __repr__(self):
+        return "success"
+
+
+class Failure(object):
+
+    def __init__(self, code):
+        self.code = code
+
+    def __repr__(self):
+        return "failure"
+
+
+class HttpAgent(Agent):
+
+    def is_valid(self):
+        checks = [("url" in self._data)
+                  ]
+        return all(checks)
+
+    @property
+    def url(self):
+        return self._data["url"]
+
+    @property
+    def headers(self):
+        return self._data.get("headers", [])
+
+    @property
+    def cookies(self):
+        return self._data.get("cookies", [])
+
+    def translate_status(self, code):
+        if code in [200, 201, 202, 204, 205, 206]:
+            return Success()
+        else:
+            return Failure(code)
+
+    async def process(self, event_fn, logger):
+        ts = time()
+        state = None
+        async with aiohttp.ClientSession() as session:
+            async with session.get(self.url) as resp:
+                state = self.translate_status(resp.status)
+        te = time()
+        span = te - ts
+        logger.debug("Request to {url} returned status code {code} (as {state}) "
+                     "in {span} milliseconds.".format(url=url,
+                                                      code=resp.status,
+                                                      state=state,
+                                                      span=span))
+
+        event_fn(metric_f=span,
+                 state=state,
+                 description=url)
