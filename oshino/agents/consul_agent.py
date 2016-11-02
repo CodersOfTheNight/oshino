4e3b1fe78f2983bd403613f6710577698d441730
Šarūnas Navickas <sarunas.navickas@adform.com>
2016-11-02 07:39:19 +0200

    Consul urls

--- oshino/agents/consul_agent.py
+++ oshino/agents/consul_agent.py
@@ -4,6 +4,29 @@ from . import Agent
 class ConsulAgent(Agent):
 
     @property
+    def leader_url(self):
+        return "http://{host}:{port}/v1/status/leader".format(host=self.host,
+                                                              port=self.port)
+
+    @property
+    def services_url(self):
+        return ("http://{host}:{port}/v1/catalog/services"
+                .format(host=self.host,
+                        port=self.port))
+
+    @property
+    def nodes_url(self):
+        return ("http://{host}:{port}/v1/catalog/nodes"
+                .format(host=self.host,
+                        port=self.port))
+
+    @property
+    def health_url(self):
+        return ("http://{host}:{port}/v1/health/service/"
+                .format(host=self.host,
+                        port=self.port))
+
+    @property
     def host(self):
         return self._data.get("host", "localhost")
 
