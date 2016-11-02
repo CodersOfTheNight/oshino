b2fbd89def1cd48112a035a7698236e742a5e2d5
Šarūnas Navickas <sarunas.navickas@adform.com>
2016-11-02 08:29:32 +0200

    Handle prefix inside agent

--- oshino/agents/__init__.py
+++ oshino/agents/__init__.py
@@ -3,6 +3,10 @@ class Agent(object):
     def __init__(self, cfg):
         self._data = cfg
 
+    @property
+    def prefix(self):
+        return "{0}.".format(self._data["name"])
+
     async def process(self, event_fn, logger):
         pass
 
