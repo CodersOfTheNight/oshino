7ef25018a85d0d815b238529551bd7bc9173c440
Šarūnas Navickas <sarunas.navickas@adform.com>
2016-11-15 08:12:08 +0200

    Simplify agent

--- oshino/agents/subprocess_agent.py
+++ oshino/agents/subprocess_agent.py
@@ -1,7 +1,7 @@
 import asyncio
 
 from . import Agent
-from asyncio.subprocess import Pipe
+from asyncio.subprocess import PIPE
 
 
 class SubprocessAgent(Agent):
@@ -15,13 +15,10 @@ class SubprocessAgent(Agent):
 
     async def process(self, event_fn):
         logger = self.get_logger()
-
-        proc = await asyncio.create_subprocess_exec(self.script,
-                                                    stdout=PIPE)
-        stdout, stderr = await proc.communicate()
-        state = "ok" if proc.returncode == 0 else "failure"
-        event_fn(self.prefix,
+        proc = await asyncio.create_subprocess_shell(self.script)
+        exitcode = await proc.wait()
+        state = "ok" if exitcode == 0 else "failure"
+        event_fn(service=self.prefix,
                  state=state,
-                 metric_f=1.0,
-                 description="Stdout: {0}, Stderr: {1}".format(stdout, stderr)
+                 metric_f=1.0
                  )
