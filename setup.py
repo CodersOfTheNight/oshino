595a2d3130789f248863763da55301b91cbad364
Šarūnas Navickas <sarunas.navickas@adform.com>
2016-11-26 09:13:06 +0200

    Reorganize requirements

--- setup.py
+++ setup.py
@@ -2,15 +2,11 @@
 # -*- coding: UTF-8 -*-
 from setuptools import setup
 from pip.req import parse_requirements
-from pip.exceptions import InstallationError
 
 from oshino.version import get_version
 
-try:
-    install_reqs = list(parse_requirements("requirements.txt", session={}))
-except InstallationError:
-    # There are no requirements
-    install_reqs = []
+install_reqs = list(parse_requirements("requirements/release.txt", session={}))
+test_reqs = list(parse_requirements("requirements/test.txt", session={}))
 
 setup(name="oshino",
       version=get_version(),
@@ -19,14 +15,7 @@ setup(name="oshino",
       packages=["oshino", "oshino.core", "oshino.agents"],
       install_requires=[str(ir.req) for ir in install_reqs],
       test_suite="pytest",
-      tests_require=["pytest",
-                     "pytest-asyncio",
-                     "pytest-mock",
-                     "coverage",
-                     "mock",
-                     "flake8",
-                     "stubilous",
-                     "flask"],
+      tests_require=[str(tr.req) for tr in test_reqs],
       setup_requires=["pytest-runner"],
       entry_points={'console_scripts': ['oshino = oshino.run:main'
                                         ]
