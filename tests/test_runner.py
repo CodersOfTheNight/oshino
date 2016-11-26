67f3d3db568921f8deca0f68447498b84077e13c
Šarūnas Navickas <sarunas.navickas@adform.com>
2016-11-26 12:48:55 +0200

    Make event loop work for runner

--- tests/test_runner.py
+++ tests/test_runner.py
@@ -1,10 +1,19 @@
+import asyncio
+
 from pytest import mark, raises
 from oshino.run import main
 from mock import patch
 
 
+def create_loop():
+    loop = asyncio.new_event_loop()
+    asyncio.set_event_loop(loop)
+    return loop
+
+
 @mark.integration
 @patch("oshino.core.heart.forever", lambda: False)
+@patch("oshino.core.heart.create_loop", create_loop)
 def test_startup():
     with raises(SystemExit):
         main(("--config", "tests/data/test_config.yml", ))
