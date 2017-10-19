2de8ff7f07ee62596cece1cd5096be66410efe42
Šarūnas Navickas <s.navickas@adform.com>
2017-10-19 13:15:12 +0300

    Basic math function

--- oshino/augments/__init__.py
+++ oshino/augments/__init__.py
@@ -1,6 +1,26 @@
+from time import time
+
 class AugmentBase(object):
     def __init__(self, data):
         self._data = data
 
     def activate(self, client, g):
         pass
+
+    def send_event(self, client, **kwargs):
+        tags = [self.tag] if self.tag else []
+        if "tags" in kwargs:
+            for tag in tags:
+                kwargs["tags"].append(tag)
+        else:
+            kwargs["tags"] = tags
+
+        if "time" not in kwargs:
+            kwargs["time"] = int(time())
+
+        client.event(**kwargs)
+
+
+def consume(q):
+    while not q.empty():
+        yield q.get()
--- oshino/augments/stats.py
+++ oshino/augments/stats.py
@@ -1,11 +1,29 @@
-from . import AugmentBase
+from queue import Queue
+
+from . import AugmentBase, consume
 
 
 class MovingAverage(AugmentBase):
+
+    def __init__(self, *args, **kwargs):
+        super(MovingAverage, self).__init__(*args, **kwargs)
+        self.q = Queue()
 
     @property
     def step(self):
         self._data.get("step", 5)
 
     def activate(self, client, g):
-        pass
+        step = self.step
+
+        while True:
+            for i in range(0, step):
+                for event in g:
+                    self.q.put(event)
+
+            data = list(map(lambda x: x.metric, consume(self.q)))
+            output = sum(data) / len(data)
+
+            self.send_event(service=self.prefix,
+                            metric_f=output,
+                            state="ok")
