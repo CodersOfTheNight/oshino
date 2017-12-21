5e202a91df106e26adfbd2c7766c8990afeeb749
Šarūnas Navickas <s.navickas@adform.com>
2017-12-21 08:05:29 +0200

    Style fix

--- oshino/util.py
+++ oshino/util.py
@@ -5,5 +5,9 @@ def dynamic_import(path):
     module, builder = path.rsplit(".", 1)
     return getattr(__import__(module, fromlist=[builder]), builder)
 
+
 def current_ts():
+    """
+    Just gives current timestamp.
+    """
     return int(time() * 1000)
