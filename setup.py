36008e48676b229c31f3083ad5c0d338f1f344fd
Šarūnas Navickas <zaibacu@gmail.com>
2018-11-21 15:48:38 +0200

    Fix requirements parse

--- setup.py
+++ setup.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python
-# -*- coding: UTF-8 -*-
 from setuptools import setup, find_packages
-from pip.req import parse_requirements
-
+try:
+    from pip._internal.req import parse_requirements
+except:
+    from pip.req import parse_requirements
 from oshino.version import get_version
 
 install_reqs = list(parse_requirements("requirements/release.txt", session={}))
